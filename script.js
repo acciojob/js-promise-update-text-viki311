381497b714e22122946ab0ade2eff633942eea28
viki311 <47682424+viki311@users.noreply.github.com>
2023-03-14 19:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function ans{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000);
+	})
+}
+	ans().then((res)=>document.getElementById("output").innerHTML=`<h1>${res}</h1>`)
